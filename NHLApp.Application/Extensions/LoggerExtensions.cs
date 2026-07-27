ca1b068385520e2906862ef035e90c33adf28579
Kevin Marwick <kevin.marwick@hotmail.com>
2026-07-26 21:04:56 -0400

    Ajout d'un LoggerExtension
    Permet d'ajouter une couleur au texte du logger dans la console

---
+++ NHLApp.Application/Extensions/LoggerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NHLApp.Application.Extensions
+{
+    public static class LoggerExtensions
+    {
+        public static void LogInformation(this ILogger logger, string message, ConsoleColor consoleColor, params object?[] args)
+        {
+            // Save the current color to restore it later
+            var previousColor = Console.ForegroundColor;
+
+            try
+            {
+                Console.ForegroundColor = consoleColor;
+
+                // Call the standard logger
+                logger.LogInformation("{Message}", message);
+            }
+            finally
+            {
+                // Always reset the color, even if logging throws an exception
+                Console.ForegroundColor = previousColor;
+            }
+        }
+    }
+}
